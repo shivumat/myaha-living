33ca0d4c5aa494a84f9932259bddf4a187b46e5b
Shivank Mathur <shivumat@gmail.com>
2025-02-11 18:07:01 +0530

    components

---
+++ ui/coming-soon.tsx
@@ -0,0 +1,92 @@
+import newStyled from '@emotion/styled';
+import Image from 'next/image';
+import { useState } from 'react';
+import WaitlistForm from './waitlist-form';
+
+const Header1 = newStyled.span`
+    font-family: Roboto Flex;
+    font-size: 24px;
+    color: #ffffff;
+    margin: 0 12px;
+    text-align: center;
+`;
+
+const SubHeader1 = newStyled.span`
+    font-family: Roboto Flex;
+    font-style: italic;
+    font-size: 16px;
+    color: #ffffff;
+    margin: 0 12px;
+    text-align: center;
+`;
+
+const CTA1 = newStyled.span`
+    cursor: pointer;
+    font-family: Roboto Flex;
+    font-size: 20px;
+    color: #ffffff;
+    text-decoration: underline;
+    text-align: center;
+`;
+
+const StyledContainer = newStyled.div`
+    padding: 24px;
+    display: flex;
+    height: 100%;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 8px;
+`;
+
+const ComingSoonComponent = () => {
+  const [satge, setStage] = useState(0);
+
+  const DefaultStage = (
+    <div className="flex h-80 flex-col items-center justify-center gap-y-4 ">
+      <Header1>Crafting emotions, not just essentials – Coming Soon</Header1>
+      <SubHeader1>
+        Be among the first to explore our exclusive collection. Join the
+        waitlist and unlock an early bird discount.
+      </SubHeader1>
+      <CTA1 onClick={() => setStage(1)}>Join the Waitlist</CTA1>
+    </div>
+  );
+
+  const VerifiedStage = (
+    <div className="flex h-80 flex-col items-center justify-center gap-y-4 ">
+      <Header1>You’re In! 🎉</Header1>
+      <SubHeader1>
+        Welcome to Myaha! You’re now on the waitlist. The top 100 users will
+        receive an exclusive early bird discount.
+      </SubHeader1>
+    </div>
+  );
+
+  const renderStage = () => {
+    switch (satge) {
+      case 0:
+        return DefaultStage;
+      case 1:
+        return <WaitlistForm updateStage={() => setStage(2)} />;
+      case 2:
+        return <WaitlistForm updateStage={() => setStage(2)} />;
+      default:
+        return VerifiedStage;
+    }
+  };
+
+  return (
+    <StyledContainer>
+      <Image
+        src="/images/myaha_logo.png"
+        alt="Myaha Living"
+        width={200}
+        height={200}
+      />
+      {renderStage()}
+    </StyledContainer>
+  );
+};
+
+export default ComingSoonComponent;
---
+++ ui/waitlist-form.tsx
@@ -0,0 +1,106 @@
+import newStyled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+
+const Header1 = newStyled.span`
+    font-family: Roboto Flex;
+    font-size: 20px;
+    color: #ffffff;
+    margin: 0 12px;
+    text-align: center;
+`;
+
+const SubHeader1 = newStyled.span`
+    font-family: Roboto Flex;
+    font-style: italic;
+    font-size: 16px;
+    color: #ffffff;
+    margin: 0 12px;
+    text-align: center;
+`;
+
+const CTA1 = newStyled.span`
+    cursor: pointer;
+    font-family: Roboto Flex;
+    font-size: 20px;
+    color: #ffffff;
+    text-decoration: underline;
+    text-align: center;
+`;
+
+export default function WaitlistForm(props: { updateStage: () => void }) {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [otp, setOtp] = useState('');
+  const [step, setStep] = useState(1);
+
+  useEffect(() => {
+    setEmail('');
+    setName('');
+    setOtp('');
+    setStep(1);
+  }, []);
+
+  const sendOtpServer = async () => {
+    console.log(email, name);
+    const res = await fetch('/api/waitlist/sendOtp', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, name }),
+    });
+
+    console.log(res);
+    setStep(2);
+  };
+
+  const verifyOtpServer = async () => {
+    const res = await fetch('/api/waitlist/sendOtp', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, otp }),
+    });
+
+    console.log(res);
+    props.updateStage();
+  };
+
+  return (
+    <div>
+      {step === 1 ? (
+        <div className="flex h-80 flex-col items-center justify-center gap-y-4 ">
+          <Header1>
+            Let us know about you and we will make sure your are the first one
+            to checkout the Designs to Cherish.
+          </Header1>
+          <input
+            type="text"
+            placeholder="Your Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <CTA1 onClick={sendOtpServer}>Send OTP</CTA1>
+        </div>
+      ) : (
+        <div className="flex h-80 flex-col items-center justify-center gap-y-4 ">
+          <Header1>You're Almost There!</Header1>
+          <SubHeader1>
+            We’ve sent an OTP to your email. Enter it below to confirm your spot
+            on the waitlist.
+          </SubHeader1>
+          <input
+            type="text"
+            placeholder="Enter OTP"
+            onChange={(e) => setOtp(e.target.value)}
+            value={otp}
+          />
+          <CTA1 onClick={verifyOtpServer}>Verify OTP</CTA1>
+        </div>
+      )}
+    </div>
+  );
+}
