929e7bed4474bbcdb898007fa4dc53fd87fa9076
Shivank Mathur <shivumat@gmail.com>
2025-02-11 18:26:11 +0530

    change in response structure

--- app/api/waitlist/verifyOtp/route.ts
+++ app/api/waitlist/verifyOtp/route.ts
@@ -7,25 +7,45 @@ export const POST = async (req: Request) => {
     console.log(otp, email);
     if (!email || !otp) {
       // Render the closest `not-found.js` Error Boundary
-      return { status: false, message: 'Invalid OTP' };
+      return new Response(
+        JSON.stringify({ status: false, message: 'Wrong Payload' }),
+        {
+          status: 400,
+        },
+      );
     }
     // Generate a 6-digit OTP
     const userRef = db.doc(`waitlist_users/${email}`);
     const userSnap = await userRef.get();
 
     if (!userSnap.exists) {
-      return { status: false, message: 'User not found' };
+      return new Response(
+        JSON.stringify({ status: false, message: 'User not found' }),
+        {
+          status: 400,
+        },
+      );
     }
 
     const userData = userSnap.data();
 
     if (userData?.otp !== otp) {
-      return { status: false, message: 'Invalid OTP' };
+      return new Response(
+        JSON.stringify({ status: false, message: 'Invalid OTP' }),
+        {
+          status: 400,
+        },
+      );
     }
 
     // Mark user as verified
     await userRef.update({ verified: true });
-    return { status: true, message: 'Invalid OTP' };
+    return new Response(
+      JSON.stringify({ status: true, message: 'Valid OTP' }),
+      {
+        status: 200,
+      },
+    );
   } catch (error) {
     console.error('Error sending OTP:', error);
     notFound();
