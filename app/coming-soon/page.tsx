3d6d75e8d2996959ff709e50263c15f8440c377e
Shivank Mathur <shivumat@gmail.com>
2025-02-24 04:35:16 +0530

    size changes

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -16,6 +16,7 @@ const HeroContainer = newStyled.div`
 
   @media (max-width: 800px) {
     background-position: 60% center; /* Adjust focus for mobile */
+    height: 100vh;
   }
 `;
 
@@ -28,8 +29,8 @@ const Logo = newStyled(Image)`
 const JoinCTA = newStyled.div`
   position: absolute;
   max-width: 450px;
-  bottom: 350px;
-  right: 300px;
+  top: 50%;
+  left: 60%;
   color: white;
   .heading {
     font-size: 48px;
@@ -41,8 +42,8 @@ const JoinCTA = newStyled.div`
   }
   @media (max-width: 800px) {
     max-width: 250px;
-    bottom:50%;
-    right: calc(50% - 125px);
+    top: 40%;
+    left: calc(50% - 125px);
 
     .heading, .sub-heading {
       text-align: center;
@@ -66,14 +67,14 @@ const Form = newStyled.div`
   backdrop-filter: blur(10px);
   text-align: center;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
-  bottom: 150px;
-  right: 270px;
+  top: 65%;
+  left: 60%;
   width : 475px;
   padding: 20px;
   @media (max-width: 800px) {
     width: 80%;
-    bottom: 50px;
-    right: 10%;
+    top: 70%;
+    left: 10%;
   }
 `;
 export default function ComingSoonPage() {
