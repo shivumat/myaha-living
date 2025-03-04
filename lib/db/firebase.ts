9f135e67b8ef6196fa1a18dbb87e88ef470354b6
Shivank Mathur <shivumat@gmail.com>
2025-03-04 09:46:32 +0530

    minor ui bug

--- lib/db/firebase.ts
+++ lib/db/firebase.ts
@@ -10,7 +10,7 @@ export class Firebase {
     if (!admin.apps.length) {
       admin.initializeApp({
         credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
+          projectId: 'myaha-waitlist',
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
           privateKey: process.env.FIREBASE_PROVATE_KEY?.replace(/\\n/g, '\n'),
         }),
