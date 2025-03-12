5e405ab8b534d62c8932e656abe8b1e61cdf9fd9
Shivank Mathur <shivumat@gmail.com>
2025-03-13 00:22:10 +0530

    Footer issue

--- ui/components/ProductWithDetails.tsx
+++ ui/components/ProductWithDetails.tsx
@@ -38,7 +38,11 @@ const ProductWithDetails = (props: { product: Product; isEven: boolean }) => {
     <div
       style={{
         display: 'flex',
-        flexDirection: isMobile ? 'column' : !isEven ? 'row' : 'row-reverse',
+        flexDirection: isMobile
+          ? 'column-reverse'
+          : !isEven
+            ? 'row'
+            : 'row-reverse',
         gap: '20px',
         justifyContent: 'space-between',
         padding: '20px',
