ee8f7adc4941b29703f5effa8587083df07124fd
Shivank Mathur <shivumat@gmail.com>
2025-03-17 23:43:16 +0530

    images

--- ui/components/Carousel.tsx
+++ ui/components/Carousel.tsx
@@ -84,7 +84,7 @@ const Carousel = (props: {
     if (!autoScroll || dotMap.length <= 1) return;
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % dotMap.length);
-    }, 6000);
+    }, 4500);
     return () => clearInterval(interval);
   }, [autoScroll, dotMap.length]);
 
