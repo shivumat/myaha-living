f78f9a43ad9c71e474463cf0ae07c6d468a420c5
Shivank Mathur <shivumat@gmail.com>
2025-04-02 15:47:31 +0530

    meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,30 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '629855199812466');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=629855199812466&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className="h-full w-full overflow-y-scroll">
         <SearchProvider>
