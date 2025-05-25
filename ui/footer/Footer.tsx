cbaf1fc6644fa9dc8b15679bf584849fcae059cc
Shivank Mathur <shivumat@gmail.com>
2025-05-25 08:41:38 +0530

    Footer changes

--- ui/footer/Footer.tsx
+++ ui/footer/Footer.tsx
@@ -1,15 +1,25 @@
 'use client';
 import { useIsMobile } from '#/hooks/useMobile';
+import { mergeHexColorsWithWeights } from '#/lib/util';
 import newStyled from '@emotion/styled';
 import { useRouter } from 'next/navigation';
+import {
+  LiaInstagram,
+  LiaLinkedinIn,
+  LiaPinterestSquare,
+  LiaTwitter,
+} from 'react-icons/lia';
 import Colors from '../colors/colors';
 import MyahaLogo from '../svg/myaha-logo';
 
 const FooterContainer = newStyled.div`
   width: 100%;
   display: flex;
-  background-color: ${Colors.black};
-  color: ${Colors.white};
+  background-color: ${mergeHexColorsWithWeights([
+    { hex: Colors.white, weight: 1 },
+    { hex: Colors.black, weight: 1 },
+  ])};
+  color: ${Colors.black};
   padding: 40px;
   @media (max-width: 800px) {
     flex-direction: column;
@@ -27,7 +37,7 @@ const TextContainer = newStyled.div`
 `;
 
 const StyleLogo = newStyled(MyahaLogo)`
-  opacity: 0.3;
+  filter: invert(1);
   position: relative;
   right: 30px;
   @media (max-width: 800px) {
@@ -65,7 +75,7 @@ const Footer = () => {
               }}
             >
               <div
-                className="clickable"
+                className="clickable hover_underline"
                 onClick={() => route.push('/contact')}
                 style={{
                   display: 'block',
@@ -76,7 +86,7 @@ const Footer = () => {
                 CONTACT
               </div>
               <div
-                className="clickable"
+                className="clickable hover_underline"
                 onClick={() => route.push('/terms')}
                 style={{
                   display: 'block',
@@ -87,7 +97,7 @@ const Footer = () => {
                 T&C
               </div>
               <div
-                className="clickable"
+                className="clickable hover_underline"
                 onClick={() => route.push('/privacy')}
                 style={{
                   display: 'block',
@@ -98,7 +108,7 @@ const Footer = () => {
                 PRIVACY
               </div>
               <div
-                className="clickable"
+                className="clickable hover_underline"
                 onClick={() => route.push('/shipping')}
                 style={{
                   display: 'block',
@@ -109,7 +119,7 @@ const Footer = () => {
                 SHIPPING
               </div>
               <div
-                className="clickable"
+                className="clickable hover_underline"
                 onClick={() => route.push('cancellation')}
                 style={{
                   display: 'block',
@@ -129,27 +139,18 @@ const Footer = () => {
               rel="noopener noreferrer"
               style={{ padding: '2px', borderRadius: '2px' }}
             >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Instagram-Gradient-Logo-PNG.png"
-                alt="Instagaram"
-                style={{ width: '15px', height: '15px' }}
-              />
+              <LiaInstagram />
             </a>
             <a
               href={`https://x.com/MyahaIndia`}
               target="_blank"
               rel="noopener noreferrer"
               style={{
-                backgroundColor: Colors.white,
                 padding: '2px',
                 borderRadius: '2px',
               }}
             >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Twitter_X.png"
-                alt="Twitter"
-                style={{ width: '15px', height: '15px' }}
-              />
+              <LiaTwitter />
             </a>
             <a
               href={`https://in.pinterest.com/06espov87v6s4p8p49qexdj4fnqr6h/`}
@@ -157,11 +158,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               style={{ padding: '2px', borderRadius: '2px' }}
             >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png"
-                alt="Pinterest"
-                style={{ width: '15px', height: '15px' }}
-              />
+              <LiaPinterestSquare />
             </a>
             <a
               href={`https://www.linkedin.com/company/myaha-home/`}
@@ -169,11 +166,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               style={{ padding: '2px', borderRadius: '2px' }}
             >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Linkedin-logo-blue-In-square-40px.png"
-                alt="Linkedin"
-                style={{ width: '15px', height: '15px' }}
-              />
+              <LiaLinkedinIn />
             </a>
           </div>
         </div>
@@ -196,7 +189,6 @@ const Footer = () => {
       <div
         style={{
           display: 'flex',
-          borderBottom: `1px solid ${Colors.white}`,
           width: '100%',
           gap: '100px',
           padding: '20px 0px',
@@ -204,60 +196,6 @@ const Footer = () => {
       >
         <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
           <StyleLogo height={'120'} width={'350'} />
-          <div style={{ display: 'flex', gap: '20px' }}>
-            <a
-              href={`https://www.instagram.com/myaha.co?igsh=MXVneTY0cnl3a2ZwNQ==`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ padding: '5px', borderRadius: '5px' }}
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Instagram-Gradient-Logo-PNG.png"
-                alt="Instagaram"
-                style={{ width: '30px', height: '30px' }}
-              />
-            </a>
-            <a
-              href={`https://x.com/MyahaIndia`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{
-                backgroundColor: Colors.white,
-                padding: '5px',
-                borderRadius: '5px',
-              }}
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Twitter_X.png"
-                alt="Twitter"
-                style={{ width: '30px', height: '30px' }}
-              />
-            </a>
-            <a
-              href={`https://in.pinterest.com/06espov87v6s4p8p49qexdj4fnqr6h/`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ padding: '5px', borderRadius: '5px' }}
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png"
-                alt="Pinterest"
-                style={{ width: '30px', height: '30px' }}
-              />
-            </a>
-            <a
-              href={`https://www.linkedin.com/company/myaha-home/`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ padding: '5px', borderRadius: '5px' }}
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Linkedin-logo-blue-In-square-40px.png"
-                alt="Linkedin"
-                style={{ width: '30px', height: '30px' }}
-              />
-            </a>
-          </div>
           <TextContainer
             style={{
               textAlign: 'left',
@@ -289,7 +227,7 @@ const Footer = () => {
             }}
           >
             <div
-              className="clickable"
+              className="clickable hover_underline"
               onClick={() => route.push('/contact')}
               style={{ display: 'block', fontSize: '1.3em' }}
             >
@@ -300,6 +238,43 @@ const Footer = () => {
             </div>
             <div style={{ display: 'block', fontSize: '1.2em' }}>
               hello@myahaliving.com{' '}
+            </div>
+            <div style={{ display: 'flex', gap: '5px', alignItems: 'center' }}>
+              <a
+                href={`https://www.instagram.com/myaha.co?igsh=MXVneTY0cnl3a2ZwNQ==`}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ padding: '2px', borderRadius: '2px' }}
+              >
+                <LiaInstagram size={'32px'} />
+              </a>
+              <a
+                href={`https://x.com/MyahaIndia`}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  padding: '2px',
+                  borderRadius: '2px',
+                }}
+              >
+                <LiaTwitter size={'32px'} />
+              </a>
+              <a
+                href={`https://in.pinterest.com/06espov87v6s4p8p49qexdj4fnqr6h/`}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ padding: '2px', borderRadius: '2px' }}
+              >
+                <LiaPinterestSquare size={'32px'} />
+              </a>
+              <a
+                href={`https://www.linkedin.com/company/myaha-home/`}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ padding: '2px', borderRadius: '2px' }}
+              >
+                <LiaLinkedinIn size={'32px'} />
+              </a>
             </div>
           </TextContainer>
         </div>
@@ -314,14 +289,14 @@ const Footer = () => {
             }}
           >
             <div
-              className="clickable"
+              className="clickable hover_underline"
               onClick={() => route.push('/about-us')}
               style={{ display: 'block', fontSize: '1.3em' }}
             >
               About us
             </div>
             <div
-              className="clickable"
+              className="clickable hover_underline"
               onClick={() => route.push('/collaborate')}
               style={{ display: 'block', fontSize: '1.3em' }}
             >
@@ -340,28 +315,28 @@ const Footer = () => {
             }}
           >
             <div
-              className="clickable"
+              className="clickable hover_underline"
               onClick={() => route.push('/terms')}
               style={{ display: 'block', fontSize: '1.3em' }}
             >
               Terms and Conditions
             </div>
             <div
-              className="clickable"
+              className="clickable hover_underline"
               onClick={() => route.push('/privacy')}
               style={{ display: 'block', fontSize: '1.3em' }}
             >
               Privacy Policy
             </div>
             <div
-              className="clickable"
+              className="clickable hover_underline"
               onClick={() => route.push('cancellation')}
               style={{ display: 'block', fontSize: '1.3em' }}
             >
               Cancellation and Refund
             </div>
             <div
-              className="clickable"
+              className="clickable hover_underline"
               onClick={() => route.push('/shipping')}
               style={{ display: 'block', fontSize: '1.3em' }}
             >
