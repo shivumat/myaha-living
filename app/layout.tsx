29350baa5ce4d70125f6331d69f0d5b5cd8efbde
Shivank Mathur <shivumat@gmail.com>
2025-08-14 18:42:06 +0530

    gtags

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,22 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QZBPG4MM65"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QZBPG4MM65');
+        `,
+          }}
+        />
         <noscript>
           <img
             height="1"
