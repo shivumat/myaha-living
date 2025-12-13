6bcc2a50c5f089069a27e9e49f8646e70043debb
Shivank Mathur <shivumat@gmail.com>
2025-12-13 19:52:43 +0530

    reveiws

--- ui/home/Testimonial/index.tsx
+++ ui/home/Testimonial/index.tsx
@@ -35,7 +35,71 @@ const testimonials: Testimonial[] = [
       'True to its photos, the sunset vases add elegance to any dinner party tablescape. This visually stunning piece makes for a perfect gift for any occasion.',
     images: ['/images/testimonials/4/1.jpeg', '/images/testimonials/4/2.jpeg'],
   },
+  {
+    name: 'Shivank Mathur',
+    rating: 5,
+    review:
+      'This lamp completely changed the vibe of our living room. Love the ambient vibe it adds to our space and the green colour gives a lovely pop without being too loud. One of my favourite pieces at home right now.',
+    images: ['/images/testimonials/5/1.jpeg', '/images/testimonials/5/2.jpeg'],
+  },
+  {
+    name: 'Radhika Agarwal',
+    rating: 5,
+    review:
+      'Got my hands on Divergence glasses for Diwali. Even plain drinks looked fancy in these glasses. Love how unique the designs are. We used them for mocktails at home and everyone kept asking about them.',
+    images: ['/images/testimonials/6/1.jpeg'],
+  },
   // Add more testimonials here
+
+  {
+    name: 'Vidhi Agarwal',
+    rating: 5,
+    review:
+      'I have never gotten as many compliments on piece of crockery as I have on Myaha’s divergence glasses. And if someone’s not outright appreciated them, every single person has taken a second look - the pieces are truly that unique. I’ve got my eyes on the glitch set now😛',
+    images: ['/images/testimonials/7/1.jpeg'],
+  },
+  {
+    name: 'Swati Choudhary',
+    rating: 5,
+    review:
+      'I’m genuinely loving these new decor pieces. These textured vases instantly elevated my living room! The bold red one has such a striking presence, and the off-white planter brings a calm, organic touch complementing the rug and the floor.',
+    images: ['/images/testimonials/8/1.jpeg'],
+  },
+  {
+    name: 'Rajat khandelwal',
+    rating: 5,
+    review:
+      'The light and dark green earthy planters are just beautiful. Their soft, rounded shapes just blend naturally into our space, giving the plants a warm, grounded feel. They add that subtle charm that makes the room feel more peaceful and cozy.',
+    images: ['/images/testimonials/9/1.jpeg'],
+  },
+  {
+    name: 'Vidhi Agarwal',
+    rating: 5,
+    review:
+      'Such cute little planters! They fit perfectly on my shelf and add so much character to the space. Finish feels very earthy and handmade. Also really impressed with how well they were packed everything arrived safely.',
+    images: ['/images/testimonials/10/1.jpeg'],
+  },
+  {
+    name: 'Shivank Mathur',
+    rating: 5,
+    review:
+      'I’d been searching for a tall vase for my side table and finally found one I love. This brown vase from Myaha fits my bedroom vibe perfectly and is also affordable for the size and quality of the product.',
+    images: ['/images/testimonials/11/1.jpeg'],
+  },
+  {
+    name: 'Radhika Agarwal',
+    rating: 5,
+    review:
+      "Just couldn't have my eyes off this piece the moment I saw it. It looks beautiful both with flowers and even on its own. Everyone who visits ends up asking where it’s from. Feels very premium and thoughtfully made.",
+    images: ['/images/testimonials/12/1.jpeg'],
+  },
+  {
+    name: 'Radhika Agarwal',
+    rating: 5,
+    review:
+      ' love how this Grace vase from Myaha doesn’t overpower my desk, but still adds such subtle character to it. The packaging was done really well, all the glass pieces arrived safely. Definitely ordering this one in more colours.',
+    images: ['/images/testimonials/12/1.jpeg'],
+  },
 ];
 
 const StyledContainer = styled(Container)`
